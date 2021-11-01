0a7c0fe7cb268398c5b292c1f95d79e27aabf9f5
pratamawijaya <set.mnemonix@gmail.com>
2021-11-01 08:28:51 +0700

    add features screen

---
+++ lib/src/features/cards/card_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CardScreen extends StatelessWidget {
+  const CardScreen({Key? key}) : super(key: key);
+
+  static const String routeName = "/card";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/features/list/list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
+  static const String routeName = "/list";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
